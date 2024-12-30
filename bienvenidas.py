ccea4dd97cff5dbb532796dae931ffc244fd2168
Pablo <t4skor@gmail.com>
2024-12-30 03:26:12 +0100

    Add files via upload

---
+++ bienvenidas.py
@@ -0,0 +1,47 @@
+import discord
+import aiohttp
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageOps
+
+async def send_welcome_message(member):
+    """Envía un mensaje de bienvenida y la foto de perfil a los canales especificados"""
+    welcome_channel_ids = 1322734453679980625
+    role_id = 1322741281163841568
+
+    for channel_id in welcome_channel_ids:
+        welcome_channel = member.guild.get_channel(channel_id)
+        if welcome_channel:
+            try:
+                await welcome_channel.send(f"Bienvenido {member.mention} a HYPΣɌIӨП  ɌP. ¡Esperamos que te guste!")
+
+                avatar_url = member.avatar.url
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(avatar_url) as response:
+                        if response.status == 200:
+                            avatar_bytes = await response.read()
+                            avatar_image = Image.open(BytesIO(avatar_bytes))
+
+                            background = Image.open(r"C:\Users\fanta\Desktop\Programas\Discord\Hyperion\hyperion_oficial_logo.png")
+                            background = background.resize((800, 600))
+
+                            avatar_image = avatar_image.resize((200, 200))
+                            mask = Image.new("L", avatar_image.size, 0)
+                            draw = ImageDraw.Draw(mask)
+                            draw.ellipse((0, 0, avatar_image.size[0], avatar_image.size[1]), fill=255)
+                            avatar_image.putalpha(mask)
+
+                            background.paste(avatar_image, (300, 200), avatar_image)
+
+                            output = BytesIO()
+                            background.save(output, format="PNG")
+                            output.seek(0)
+                            await welcome_channel.send(file=discord.File(output, filename="welcome_image.png"))
+            except Exception as e:
+                print(f"Error al procesar el mensaje de bienvenida: {e}")
+
+    try:
+        role = discord.utils.get(member.guild.roles, id=role_id)
+        if role:
+            await member.add_roles(role)
+    except Exception as e:
+        print(f"Error al asignar el rol: {e}")
---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import webserver
+from tickets import send_ticket_menu
+from whitelist import wl
+from bienvenidas import send_welcome_message
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True 
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_member_join(member):
+    """Llamada cuando un miembro se une al servidor."""
+    await send_welcome_message(member)
+
+@bot.event
+async def on_ready():
+    """Llamada cuando el bot está listo y conectado."""
+
+    await bot.change_presence(activity=discord.Game(name="HyperionRP"))
+    
+    try:
+        synced = await bot.tree.sync()
+        print(f"Comandos slash sincronizados: {len(synced)} comandos")
+    except Exception as e:
+        print(f"Error al sincronizar comandos slash: {e}")
+
+    ticket_channel_id = 1322694316019290174
+    whitelist_channel_id = 1322978279921156286
+    ticket_channel = bot.get_channel(ticket_channel_id)
+    whitelist_channel = bot.get_channel(whitelist_channel_id)
+
+    if ticket_channel:
+        file_path = r"C:\Users\fanta\Desktop\Programas\Discord\Hyperion\python\logo.png"
+        await send_ticket_menu(ticket_channel, file_path)  
+    
+    if whitelist_channel:
+        await wl(whitelist_channel)
+    else:
+        print(f"Error: Canal de whitelist con ID {whitelist_channel_id} no encontrado.")
+
+webserver.keep_alive()
+bot.run(DISCORD_TOKEN)
